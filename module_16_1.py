414c7046cdd59b2abc62bd2087a7051d65134406
Akella348 <akella348@gmail.com>
2024-12-05 17:02:03 +0400

    3 iteration

--- module_16_1.py
+++ module_16_1.py
@@ -4,15 +4,15 @@ app = FastAPI()
 
 @app.get("/")
 async def home_page():
-    return {"message":  "Главная страница"}
+    return "Главная страница"
 
 @app.get("/user/admin")
 async def admin_enter():
-    return {"message":  "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def user_enter(user_id: int):
-    return {"message":  f"Вы вошли как пользователь №{user_id}"}
+    return f"Вы вошли как пользователь №{user_id}"
 
 @app.get("/user")  # исправил
 async def user_info(username: str, age: int):
